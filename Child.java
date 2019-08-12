946d343ed2f58794a83658a5eb20c727534aa1f9
mark-renrgyx <53844281+mark-renrgyx@users.noreply.github.com>
2019-08-12 13:48:32 -0400

    complete--there is no output to console for this assignment

---
+++ Child.java
@@ -0,0 +1,15 @@
+package week1.day5.assignment1;
+
+/**
+ * Child class to implement abstract methods defined in {@link Parent}
+ * @author Mark
+ *
+ */
+public class Child extends Parent {
+	private String name = "baby";
+
+	public String getName() {
+		return name;
+	}
+	
+}
---
+++ Parent.java
@@ -0,0 +1,10 @@
+package week1.day5.assignment1;
+
+/**
+ * Arbitrary "parent" class
+ * @author Mark
+ *
+ */
+public abstract class Parent {
+	public abstract String getName();
+}
